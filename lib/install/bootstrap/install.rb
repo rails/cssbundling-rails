b451f5d8e4b162af2b67e0b0aded872aa84b89a7
andreyazimov <azimov.andrey@gmail.com>
2021-09-08 16:18:37 +0700

    changed version of bootstrap to 5.1.1 and Popperjs/core to 2.10.1

--- lib/install/bootstrap/install.rb
+++ lib/install/bootstrap/install.rb
@@ -1,8 +1,8 @@
-say "Install Bootstrap 5 with Popperjs"
+say "Install Bootstrap 5 with Popperjs/core"
 copy_file "#{__dir__}/bootstrap_js_files.js", "bootstrap_js_files.js"
 copy_file "#{__dir__}/application.scss", "app/assets/stylesheets/application.scss"
 
-run "yarn add bootstrap@5.0.0-beta2"
-run "yarn add @popperjs/core@2.0.0-alpha.1"
+run "yarn add bootstrap@5.1.1"
+run "yarn add @popperjs/core@2.10.1"
 
 run %(npm set-script build:css "sass ./app/assets/stylesheets/application.scss ./app/assets/builds/application.css --no-source-map")
