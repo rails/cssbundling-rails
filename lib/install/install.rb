1ed4dc57a8a60abc6e833380be37716cd280b406
David Heinemeier Hansson <david@loudthinking.com>
2021-09-08 13:25:50 +0200

    Add bin/dev to run multiple processes

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -14,3 +14,16 @@ unless Rails.root.join("package.json").exist?
   say "Add default package.json"
   copy_file "#{__dir__}/package.json", "package.json"
 end
+
+if Rails.root.join("Procfile.dev").exist?
+  append_to_file "Procfile.dev", "css: yarn build:css --watch"
+else
+  say "Add default Procfile.dev"
+  copy_file "#{__dir__}/Procfile.dev", "Procfile.dev"
+
+  say "Ensure foreman is install"
+  run "gem install foreman"
+end
+
+say "Add bin/dev to start foreman"
+copy_file "#{__dir__}/dev", "bin/dev"
