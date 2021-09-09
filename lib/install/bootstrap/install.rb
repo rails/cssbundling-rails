712e7b988723aa1e89c5bc102744e097fa17dfb7
andreyazimov <azimov.andrey@gmail.com>
2021-09-09 19:37:45 +0700

    added say "Add build:css script" to install.rb

--- lib/install/bootstrap/install.rb
+++ lib/install/bootstrap/install.rb
@@ -5,4 +5,5 @@ copy_file "#{__dir__}/application.scss", "app/assets/stylesheets/application.scs
 run "yarn add bootstrap@latest"
 run "yarn add @popperjs/core@latest"
 
+say "Add build:css script"
 run %(npm set-script build:css "sass ./app/assets/stylesheets/application.scss ./app/assets/builds/application.css --no-source-map")
