b71d4b2f06fc3ef55edcfc776b9a48d15d43e0e5
andreyazimov <azimov.andrey@gmail.com>
2021-09-09 12:45:40 +0700

    chanaged bootstrap and popper spefic version to @latest so it will be always up to date

--- lib/install/bootstrap/install.rb
+++ lib/install/bootstrap/install.rb
@@ -1,8 +1,8 @@
-say "Install Bootstrap 5 with Popperjs/core"
+say "Install Bootstrap with Popperjs/core"
 copy_file "#{__dir__}/bootstrap_js_files.js", "bootstrap_js_files.js"
 copy_file "#{__dir__}/application.scss", "app/assets/stylesheets/application.scss"
 
-run "yarn add bootstrap@5.1.1"
-run "yarn add @popperjs/core@2.10.1"
+run "yarn add bootstrap@latest"
+run "yarn add @popperjs/core@latest"
 
 run %(npm set-script build:css "sass ./app/assets/stylesheets/application.scss ./app/assets/builds/application.css --no-source-map")
