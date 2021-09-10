c200a934aa6168ab8ea09eabfce400ef9dae8fff
David Heinemeier Hansson <david@loudthinking.com>
2021-09-10 10:49:38 +0200

    Match existing conventions and add the JavaScript needs to the default entry point

--- lib/install/bootstrap/install.rb
+++ lib/install/bootstrap/install.rb
@@ -1,9 +1,15 @@
 say "Install Bootstrap with Popperjs/core"
-copy_file "#{__dir__}/bootstrap.js", "bootstrap.js"
-copy_file "#{__dir__}/application.scss", "app/assets/stylesheets/application.scss"
+copy_file "#{__dir__}/application.bootstrap.scss",
+   "app/assets/stylesheets/application.bootstrap.scss"
+copy_file "#{__dir__}/bootstrap.js", "app/javascript/bootstrap.js"
+run "yarn add sass bootstrap @popperjs/core"
 
-run "yarn add bootstrap@latest"
-run "yarn add @popperjs/core@latest"
+if Rails.root.join("app/javascript/application.js").exist?
+  say "Appending Bootstrap JavaScript import to default entry point"
+  append_to_file "app/javascript/application.js", %(import "./bootstrap"\n)
+else
+  say %(Add import "./bootstrap" to your entry point JavaScript file), :red
+end
 
 say "Add build:css script"
-run %(npm set-script build:css "sass ./app/assets/stylesheets/application.scss ./app/assets/builds/application.css --no-source-map")
+run %(npm set-script build:css "sass ./app/assets/stylesheets/application.bootstrap.scss ./app/assets/builds/application.css --no-source-map")
