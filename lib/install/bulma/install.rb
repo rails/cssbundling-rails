9d57cae207d2de55244e2101a8c583f9fb34a4af
thomasvanholder <thomasvanholder@hey.com>
2021-09-29 19:06:10 +0100

    adding missing curly bracket

--- lib/install/bulma/install.rb
+++ lib/install/bulma/install.rb
@@ -7,7 +7,7 @@ say "Add build:css script"
 build_script = "sass ./app/assets/stylesheets/application.bulma.scss ./app/assets/builds/application.css --no-source-map --load-path=node_modules"
 
 if (`npx -v`.to_f < 7.1 rescue "Missing")
-  say %(Add "scripts": { "build:css": "#{build_script}" to your package.json), :green
+  say %(Add "scripts": { "build:css": "#{build_script}" } to your package.json), :green
 else
   run %(npm set-script build:css "#{build_script}")
 end
