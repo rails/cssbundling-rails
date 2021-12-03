a79fbc1509ddbd279cf130135e4ab10fc8fb83ba
David Heinemeier Hansson <david@hey.com>
2021-12-03 11:40:56 +0100

    Always run build:css on install
    So application.css is never missing

--- lib/install/bootstrap/install.rb
+++ lib/install/bootstrap/install.rb
@@ -17,4 +17,5 @@ if (`npx -v`.to_f < 7.1 rescue "Missing")
   say %(Add "scripts": { "build:css": "#{build_script}" } to your package.json), :green
 else
   run %(npm set-script build:css "#{build_script}")
+  run %(yarn build:css)
 end
