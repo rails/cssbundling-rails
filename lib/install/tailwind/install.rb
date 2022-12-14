c89743271d48a1384d5c1a4afc2b0053bd55827f
David Heinemeier Hansson <david@loudthinking.com>
2022-12-14 14:29:16 +0100

    Make install compatible with NPM 9+

--- lib/install/tailwind/install.rb
+++ lib/install/tailwind/install.rb
@@ -6,9 +6,13 @@ run "yarn add tailwindcss@latest postcss@latest autoprefixer@latest"
 say "Add build:css script"
 build_script = "tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify"
 
-if (`npx -v`.to_f < 7.1 rescue "Missing")
-  say %(Add "scripts": { "build:css": "#{build_script}" } to your package.json), :red
-else
+case `npx -v`.to_f
+when 7.1...8.0
   run %(npm set-script build:css "#{build_script}")
   run %(yarn build:css)
+when (8.0..)
+  run %(npm pkg set scripts.build:css="#{build_script}")
+  run %(yarn build:css)
+else
+  say %(Add "scripts": { "build:css": "#{build_script}" } to your package.json), :green
 end
