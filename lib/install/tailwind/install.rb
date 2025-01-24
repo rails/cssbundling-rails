5d844621ba73db929bb28d7a149f41a45a50d82e
Andy Leverenz <andy@justalever.com>
2025-01-24 14:10:44 -0600

    Update for Tailwind CSS v4
    Tailwind CSS v4 comes with new configurations and build scripts. This updates the installer for Tailwind to include those changes.

--- lib/install/tailwind/install.rb
+++ lib/install/tailwind/install.rb
@@ -3,11 +3,10 @@ self.extend Helpers
 
 apply "#{__dir__}/../install.rb"
 
-say "Install Tailwind (+PostCSS w/ autoprefixer)"
-copy_file "#{__dir__}/tailwind.config.js", "tailwind.config.js"
+say "Install Tailwind"
 copy_file "#{__dir__}/application.tailwind.css", "app/assets/stylesheets/application.tailwind.css"
-run "#{bundler_cmd} add tailwindcss@latest postcss@latest autoprefixer@latest"
+run "#{bundler_cmd} add tailwindcss@latest @tailwindcss/cli@latest"
 
 say "Add build:css script"
 add_package_json_script "build:css",
-  "tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify"
+  "npx @tailwindcss/cli -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify"
